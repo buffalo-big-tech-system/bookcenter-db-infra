55ea3aa2ba371945ad9c71f48c1e9477eb891def
Eldar Eyvazov <58527301+ProgrammerBuffalo@users.noreply.github.com>
2025-04-30 15:36:36 +0400

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Books (title)
+VALUES
+  ('Code Complete'),
+  ('Clean Code'),
+  ('Structure and Interpretation of Computer Programs'),
+  ('Design Patterns: Elements of Reusable Object-Oriented Software'),
+  ('Code: The Hidden Language of Computer Hardware and Software')
